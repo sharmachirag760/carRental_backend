819dec51e3d32c886463d0034f7e2389d8ebe579
Chirag Sharma <chirag@Chirags-MacBook-Air.local>
2025-05-18 15:04:04 +0530

    successfully tested for user and admin api's

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -9,3 +9,4 @@ router.get('/getprofile',protect, getProfile);
 router.put('/updateprofile',protect, updateProfile);
 router.put('/changepassword',protect, changePassword);
 module.exports = router;
+//hi
