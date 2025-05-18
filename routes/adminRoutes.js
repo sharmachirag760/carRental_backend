3c22ca6304b39d8f16d2d54dd626f608547b2a0f
Chirag Sharma <chirag@Chirags-MacBook-Air.local>
2025-05-18 15:08:55 +0530

    adding comment

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -9,4 +9,4 @@ router.get('/getprofile',protect, getProfile);
 router.put('/updateprofile',protect, updateProfile);
 router.put('/changepassword',protect, changePassword);
 module.exports = router;
-//hi
+//hi bye
